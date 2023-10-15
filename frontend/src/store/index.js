484f4ee8cee89c4c2c3a0a9af5ec6d2cfbbd679a
MuongKimhong <muongkimhong086@gmail.com>
2023-10-15 23:45:46 +1100

    user credential in vuex with mutations

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -10,11 +10,40 @@ const vuexLocal = new VuexPersistence({
 
 export default new Vuex.Store({
   state: {
-    webSocketServer: "http://localhost:3000"
+    webSocketServer: "http://localhost:3000",
+
+    user: {
+      id: null,
+      username: null,
+      profileUrl: null,
+      acceptAnonymousMessage: null,
+      accessToken: null
+    }
   },
   getters: {
   },
   mutations: {
+    updateUserCredential(state, credential) {
+      state.user.id = credential.id;
+      state.user.username = credential.username;
+      state.user.profileUrl = credential.profile_url;
+      state.user.acceptAnonymousMessage = credential.accept_anonymous_message;
+      state.user.accessToken = credential.access_token;
+    },
+
+    updateOnlyUserProfileUrl(state, newUrl) {
+      state.user.profileUrl = newUrl;
+    },
+
+    deleteUserCredential(state) {
+      state.user = {
+        id: null,
+        username: null,
+        profileUrl: null,
+        acceptAnonymousMessage: null,
+        accessToken: null
+      }
+    }
   },
   actions: {
   },
