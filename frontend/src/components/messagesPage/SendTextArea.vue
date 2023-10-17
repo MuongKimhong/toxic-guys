a3c3900b8bce88758c2c6f01d6a8ba4c05254700
MuongKimhong <muongkimhong086@gmail.com>
2023-10-17 17:06:12 +1100

    finish send text area

---
+++ frontend/src/components/messagesPage/SendTextArea.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card-actions style="background-color: rgb(95, 95, 95)">
+    <span class="mr-2">
+      <i
+        class="fas fa-images white--text"
+        id="images-icon"
+        style="font-size: 20px"
+      ></i>
+    </span>
+    <v-text-field
+      rounded
+      outlined
+      dense
+      label="Write message here"
+      dark
+      hide-details
+      append-icon="mdi-arrow-up"
+      @click:append="sendMessageIconClick()"
+    ></v-text-field>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: "SendTextArea",
+};
+</script>
+
+<style scoped>
+#images-icon {
+  cursor: pointer;
+}
+</style>
--- frontend/src/views/Messages.vue
+++ frontend/src/views/Messages.vue
@@ -3,8 +3,24 @@
     <v-row align="center" justify-content="center">
       <v-col cols="12" class="mr-auto ml-auto">
         <div>
-          <v-card class="px-0 py-0 white--text message-card">
-            <UserListInMessagePage />
+          <v-card class="px-0 py-0 white--text message-card d-flex">
+            <v-layout>
+              <UserListInMessagePage />
+              <v-card
+                class="d-flex flex-column"
+                style="
+                  background-color: white;
+                  width: 100%;
+                  border-radius: 0;
+                  padding: 5px;
+                "
+              >
+                <v-card-title>kasj</v-card-title>
+                <v-card-text>;lkasjdf</v-card-text>
+                <v-spacer></v-spacer>
+                <SendTextArea />
+              </v-card>
+            </v-layout>
           </v-card>
         </div>
       </v-col>
@@ -13,12 +29,17 @@
 </template>
 
 <script>
-import UserListInMessagePage from "../components/UserListInMessagePage.vue";
+import UserListInMessagePage from "../components/messagesPage/UserListInMessagePage.vue";
+import SendTextArea from "../components/messagesPage/SendTextArea.vue";
 
 export default {
   name: "Messages",
   components: {
     UserListInMessagePage,
+    SendTextArea,
+  },
+  methods: {
+    sendMessageIconClick: function () {},
   },
 };
 </script>
