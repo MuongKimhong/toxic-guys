ae9b7ddc9670b2a79e8fd9f3aa712ae9c5561967
MuongKimhong <muongkimhong086@gmail.com>
2023-10-17 13:10:52 +1100

    implemented SearchUser component

---
+++ frontend/src/components/SearchUser.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="8" class="mr-auto ml-auto">
+        <v-text-field
+          class="mt-2"
+          rounded
+          dense
+          label="Search users"
+          dark
+          outlined
+          append-icon="mdi-account-search"
+          v-model="searchText"
+        ></v-text-field>
+
+        <div class="text-center">
+          <small class="text-center">
+            Did you know that here you can chat with other people anonymously if
+            they are opened to anonymous conversation?
+          </small>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "SearchUser",
+
+  data: () => ({
+    searchText: "",
+  }),
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>Hello world</div>
+  <v-container>
+    <!-- not signed in -->
+    <div v-if="$store.state.user.accessToken == null">
+      <div class="mt-10">
+        <SearchUser />
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script>
+import SearchUser from "../components/SearchUser.vue";
+
 export default {
   name: "Home",
 
-  components: {},
+  components: {
+    SearchUser,
+  },
 };
 </script>
 
