b6d8fd56c23c5b32b38d44d6bd6cd2a4ba97ddeb
MuongKimhong <muongkimhong086@gmail.com>
2023-10-17 13:26:20 +1100

    Explore public groups component

---
+++ frontend/src/components/GroupCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-col cols="3">
+    <v-card flat dense class="text-center py-5 px-2 group-card">
+      <v-avatar style="background-color: grey" size="120">
+        <i class="fas fa-users white--text" style="font-size: 50px"></i>
+      </v-avatar>
+
+      <h3 class="text-center mt-2 white--text">Group Name</h3>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "GroupCard",
+};
+</script>
+
+<style scoped>
+.group-card {
+  background-color: rgb(78, 78, 78);
+  cursor: pointer;
+}
+.group-card:hover {
+  background-color: rgb(82, 82, 82);
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,6 +4,30 @@
     <div v-if="$store.state.user.accessToken == null">
       <div class="mt-10">
         <SearchUser />
+
+        <hr
+          style="
+            height: 1px;
+            background-color: grey;
+            border: none;
+            margin-top: 50px;
+            margin-bottom: 50px;
+          "
+        />
+
+        <div>
+          <h1 class="text-center mb-5">Explore public groups</h1>
+          <v-row justify="space-around">
+            <GroupCard />
+            <GroupCard />
+            <GroupCard />
+            <GroupCard />
+            <GroupCard />
+            <GroupCard />
+            <GroupCard />
+            <GroupCard />
+          </v-row>
+        </div>
       </div>
     </div>
   </v-container>
@@ -11,12 +35,14 @@
 
 <script>
 import SearchUser from "../components/SearchUser.vue";
+import GroupCard from "../components/GroupCard.vue";
 
 export default {
   name: "Home",
 
   components: {
     SearchUser,
+    GroupCard,
   },
 };
 </script>
