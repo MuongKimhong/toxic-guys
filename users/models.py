28301d6e6b74d54cfdc6dd6b6f7fd2f3b2f25f51
MuongKimhong <muongkimhong086@gmail.com>
2023-10-18 13:32:11 +1100

    implemented UserConnection model

--- users/models.py
+++ users/models.py
@@ -7,6 +7,10 @@ def profile_url(profile):
     return f"{settings.FULL_DOMAIN_PATH}{profile.url}" if profile else ""
 
 
+def date_format(date):
+    return date.strftime(""%d/%b/%Y - %I:%M %p"")
+
+
 class User(AbstractUser):
     email = models.CharField(max_length=150, unique=True)
     username = models.CharField(max_length=50, unique=True)
@@ -23,5 +27,21 @@ class User(AbstractUser):
             "email": self.email,
             "username": self.username,
             "profile" : profile_url(self.profile),
-            "created_date": self.created_date.strftime("%d/%b/%Y - %I:%M %p")
+            "created_date": date_format(self.created_date)
+        }
+
+
+class UserConnection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    connection = models.ForeignKey(User, on_delete=models.CASCADE) # friend
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.connection.username}"
+    
+    def serialize(self):
+        return {
+            "user": self.user.serialize(),
+            "connection": self.connection.serialize(),
+            "created_date": date_format(self.created_date)
         }
