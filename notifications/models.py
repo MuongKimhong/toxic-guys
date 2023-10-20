a6cd63a6e92ecdcd50178ab656cf8930de13a23e
MuongKimhong <muongkimhong086@gmail.com>
2023-10-21 01:11:19 +1100

    create Notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User, date_format
+
+
+class Notification(models.Model):
+    sender = models.ForeignKey(User, related_name="notification_sender", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name="notification_receiver", on_delete=models.CASCADE)
+    _type = models.CharField(max_length=100, default="connection") # type of notification 
+    text  = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "sender": self.sender.serialize(),
+            "receiver": self.receiver.serialize(),
+            "type": self._type,
+            "text": self.text
+            "created_date": date_format(self.created_date)
+        }
