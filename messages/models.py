f39b9956146e51779d093122e711e7a0caa368ac
MuongKimhong <muongkimhong086@gmail.com>
2023-11-02 14:09:08 +1100

    ChatRoom model

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User, date_format
+
+'''
+ChatRoom is for 2 users chatting together.
+
+Group does not need chatroom, because group
+itself act like a chatroom
+'''
+
+
+class ChatRoom(models.Model):
+    creator = models.ForeignKey(User, related_name="creator", on_delete.models.CASCADE)
+    member = models.ForeignKey(User, related_name="member", on_delete.models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "creator": self.creator.serialize(),
+            "member" : self.member.serialize(),
+            "created_date": date_format(self.created_date)
+        }
+
+
+class Message(models.Model):
+    pass
+
+
+class MessageImage(models.Model):
+    pass
