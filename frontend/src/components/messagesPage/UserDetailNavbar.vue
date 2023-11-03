88e548e28b6e7292180ae50ebb90eee7dc9cedde
MuongKimhong <muongkimhong086@gmail.com>
2023-11-03 14:31:52 +1100

    condition of chatroom type

--- frontend/src/components/messagesPage/UserDetailNavbar.vue
+++ frontend/src/components/messagesPage/UserDetailNavbar.vue
@@ -2,36 +2,52 @@
   <v-card-title
     style="background-color: rgb(95, 95, 95); border-radius: 10px; padding: 5px"
   >
-    <v-avatar
-      v-if="chatroom.creator.id == $store.state.user.id"
-      size="28"
-      color="white"
-      class="ml-2"
-    >
-      <v-img
-        v-if="chatroom.member.profile_url == ''"
-        :src="require(`../../../public/userimg.png`)"
-      ></v-img>
-      <v-img v-else :src="chatroom.member.profile_url"></v-img>
-    </v-avatar>
-    <v-avatar v-else size="28" color="white" class="ml-2">
-      <v-img
-        v-if="chatroom.creator.profile_url == ''"
-        :src="require(`../../../public/userimg.png`)"
-      ></v-img>
-      <v-img v-else :src="chatroom.creator.profile_url"></v-img>
-    </v-avatar>
+    <span v-if="chatroom.type == 'user'">
+      <v-avatar
+        v-if="chatroom.creator.id == $store.state.user.id"
+        size="28"
+        color="white"
+        class="ml-2"
+      >
+        <v-img
+          v-if="chatroom.member.profile_url == ''"
+          :src="require(`../../../public/userimg.png`)"
+        ></v-img>
+        <v-img v-else :src="chatroom.member.profile_url"></v-img>
+      </v-avatar>
+      <v-avatar v-else size="28" color="white" class="ml-2">
+        <v-img
+          v-if="chatroom.creator.profile_url == ''"
+          :src="require(`../../../public/userimg.png`)"
+        ></v-img>
+        <v-img v-else :src="chatroom.creator.profile_url"></v-img>
+      </v-avatar>
 
-    <span
-      v-if="chatroom.creator.id == $store.state.user.id"
-      class="white--text ml-2"
-      style="font-size: 16px"
-    >
-      {{ chatroom.member.username }}
+      <span
+        v-if="chatroom.creator.id == $store.state.user.id"
+        class="white--text ml-2"
+        style="font-size: 16px"
+      >
+        {{ chatroom.member.username }}
+      </span>
+      <span v-else class="white--text ml-2" style="font-size: 16px">
+        {{ chatroom.creator.username }}
+      </span>
     </span>
-    <span v-else class="white--text ml-2" style="font-size: 16px">
-      {{ chatroom.creator.username }}
+
+    <span v-else>
+      <v-avatar size="28" color="white" class="ml-2">
+        <v-img
+          v-if="chatroom.group.profile == ''"
+          :src="require(`../../../public/groupimg.png`)"
+        ></v-img>
+        <v-img v-else :src="chatroom.group.profile"></v-img>
+      </v-avatar>
+      <span class="white--text ml-2" style="font-size: 16px">
+        {{ chatroom.group.name }}
+      </span>
     </span>
+
     <v-spacer></v-spacer>
     <v-btn small text>
       <span>
